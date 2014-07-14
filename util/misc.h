06fb437996377e70886cbed06c21be8f7ddc80d4
comex <comexk@gmail.com>
2014-07-14 13:42:24 -0400

    doc update

--- util/misc.h
+++ util/misc.h
@@ -19,3 +19,27 @@ static inline size_t safe_mul(size_t a, size_t b) {
 
 #define LET_(expr, ctr) LET__(expr, ctr)
 #define LET(expr) LET_(expr, __COUNTER__)
+
+/*
+   usage: defer { foo };
+*/
+
+#define defer defer_(__COUNTER__)
+#define defer_(ctr) defer__(ctr)
+#ifdef __clang__
+__attribute__((always_inline))
+static inline void defer_cleanup(void (^*cb)()) {
+   (*cb)();
+}
+#define defer__(ctr) \
+   __attribute__((cleanup(defer_cleanup))) \
+   void (^__defer_##ctr)() = ^()
+#else
+#define defer__(ctr) \
+   __attribute_((always_inline)) \
+   auto inline void __defer_func_##ctr(); \
+   __attribute__((cleanup(__defer_func_##ctr))) \
+   int __defer_dummy_##ctr; \
+   void __defer_func_##ctr()
+#endif
+
--- util/test/runtest.sh
+++ util/test/runtest.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 set -e
-[ -z "$CC" ] && CC="/usr/local/Cellar/llvm/3.4.1/bin/clang -fsanitize=undefined"
-for file in misctest.c vectest.c; do
-   $CC -O3 -I../.. -o thetest $file
-   set +e
-   actual="$(./thetest)"
-   actual_ec=$?
-   set -e
-   expected="$(awk '/^>>/{on=0} on{print} /^expect-output<</{on=1}' $file)"
-   if [ "$actual" != "$expected" ]; then
-      echo "$file output differs.  Actual:"
-      echo "$actual"
-      echo "Expected:"
-      echo "$expected"
-      echo
-   fi
-   expected_ec="$(awk '/^expect-exit/{print $2}' $file)"
-   if [ "$actual_ec" != "$expected_ec" ]; then
-      echo "$file exit differs.  Actual: $actual_ec  Expected: $expected_ec"
-   fi
+cd "$(dirname "${BASH_SOURCE[0]}")"
+TESTS="misctest.c vectest.c"
+if [ -n "$CC" ]; then
+   CCS=("$CC")
+else
+   CCS=("/usr/local/Cellar/llvm/3.4.1/bin/clang -fsanitize=undefined"
+      "gcc-4.8"
+   )
+fi
+for file in $TESTS; do
+   for cc in "${CCS[@]}"; do
+      $cc -O3 -I../.. -o thetest $file
+      set +e
+      actual="$(./thetest)"
+      actual_ec=$?
+      set -e
+      expected="$(awk '/^>>/{on=0} on{print} /^expect-output<</{on=1}' $file)"
+      if [ "$actual" != "$expected" ]; then
+         echo "$file output differs.  Actual:"
+         echo "$actual"
+         echo "Expected:"
+         echo "$expected"
+         echo
+         bad=1
+      fi
+      expected_ec="$(awk '/^expect-exit/{print $2}' $file)"
+      if [ "$actual_ec" != "$expected_ec" ]; then
+         echo "$file exit differs.  Actual: $actual_ec  Expected: $expected_ec"
+         bad=1
+      fi
+      -z $bad
+   done
 done
 
 echo "All OK"
